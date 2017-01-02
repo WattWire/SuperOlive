b6cbcc7e182b4b8fb4ac392bbb2029c8844defde
Dave Watt <gonebikn@gmail.com>
2017-01-01 18:54:27 -0800

    Initial commit from PopTart, New Year's Day.

---
+++ src/net/wattwire/Presser.java
@@ -0,0 +1,14 @@
+package net.wattwire;
+
+/**
+ * Created by dave on 12/31/16.
+ */
+public class Presser {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("\n\n ****** Super Olive!!! *******");
+
+    }
+
+}
